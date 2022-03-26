b0eeaaa17f944240430e7893c0e49772209d59aa
farukh110 <farukhsajjad110@gmail.com>
2022-03-26 12:38:04 +0500

    created order API

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { createNewOrder, getOrderDetails, myOrders, getAllOrders, updateOrder, deleteOrder } = require("../controllers/orderController");
+const router = express.Router();
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+router.route("/order/new").post(isAuthenticatedUser, createNewOrder);
+
+router.route("/order/:id")
+.get(isAuthenticatedUser, getOrderDetails);
+
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router.route("/admin/order/:id")
+.put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+
+module.exports = router;
